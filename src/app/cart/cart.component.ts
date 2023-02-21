9d1f3bbbd24500c0a23392a19f2e774ce495bfb5
vardhan-s3 <72273277+vardhan-s3@users.noreply.github.com>
2023-02-21 17:06:36 -0600

    Add files via upload

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  constructor(
+    private cartService : CartService,
+    private formBuilder : FormBuilder,
+    ) { }
+
+  ngOnInit(): void {
+  }
+
+  checkoutForm = this.formBuilder.group({
+    'name':'',
+    'address':''
+  });
+
+  items = this.cartService.getItems();
+
+  onSubmit() : void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+}
